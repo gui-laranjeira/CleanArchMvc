8c26f193dde61194aa8ed8c0c9203e6d7c3fd569
gui-laranjeira <guilr14@gmail.com>
2023-10-29 21:34:06 -0300

    feat: adding table migrations

--- CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -17,7 +17,7 @@ namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
             builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
             builder.Property(p => p.Description).HasMaxLength(200).IsRequired();
             builder.Property(p => p.Price).HasPrecision(10, 2);
-            builder.HasOne(e => e.Category).WithMany(e => e.Products).HasForeignKey(e => e.Category.Id);
+            builder.HasOne(e => e.Category).WithMany(e => e.Products).HasForeignKey(e => e.CategoryId);
         }
     }
 }
